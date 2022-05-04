472aac8b5a3af39c8fb7c018f7ff3af2a6678d56
Rtxeon <rtxeonsuthargaming@gmail.com>
2022-05-04 08:47:23 +0000

    small update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const client = new Client({
 });
 const { readdirSync } = require("fs");
 const mongoose = require('mongoose');
+const colors = require("colors");
 
 //makng some globals
 client.commands = new Collection();
@@ -35,17 +36,17 @@ mongoose.connect(client.config.database.mongourl, {
 });
 mongoose.Promise = global.Promise;
 mongoose.connection.on('connected', () => {
-  console.log(`[MONGODB] DATABASE CONNECTED`);
+  console.log(`[MONGODB] DATABASE CONNECTED`.america);
 });
 mongoose.connection.on('err', (err) => {
-  console.log(`[MONGODB] DATABASE ERROR: \n${err.stack}`);
+  console.log(`[MONGODB] DATABASE ERROR: \n${err.stack}`.grey);
 });
 mongoose.connection.on('disconnected', () => {
-  console.log(`[MONGODB] DATABASE DISCONNECTED`);
+  console.log(`[MONGODB] DATABASE DISCONNECTED`.bgBrightRed);
 });
 
-client.on("disconnect", () => console.log("Client is disconnecting..."))
-client.on("reconnecting", () => console.log("Client reconnecting..."))
+client.on("disconnect", () => console.log(`[CLIENT] ${client.user.username} is disconnecting...`.brightRed))
+client.on("reconnecting", () => console.log(`[CLIENT] ${client.user.username} is reconnecting...`.zebra))
 client.on('warn', error => console.log(error));
 client.on('error', error => console.log(error));
 process.on('unhandledRejection', error => console.log(error));
@@ -54,6 +55,7 @@ process.on('uncaughtException', error => console.log(error))
 //Adding Events Category You Can Add More Category I Am Adding Only One Category
 readdirSync("./Events/Client/").forEach(file => {
   const event = require(`./Events/Client/${file}`);
+  console.log(`[EVENTS] Client event named ${event.name} loaded`.random);
   client.on(event.name, (...args) => event.run(client, ...args));
 });
 
@@ -63,10 +65,10 @@ readdirSync("./Commands/").forEach((dir) => {
   for (let file of commands) {
     let cmd = require(`./Commands/${dir}/${file}`);
     if (cmd.name) {
-      console.log("Loading commands");
+      console.log(`[COMMANDS] Loading command named ${cmd.name}`.brightCyan);
       client.commands.set(cmd.name, cmd);
     } else {
-      console.log(file, `missing a cmd.name, or cmd.name is not a string.`);
+      console.log("[COMMANDS]", file, `missing a cmd.name, or cmd.name is not a string.`.brightRed);
       continue;
     }
     if (cmd.aliases && Array.isArray(cmd.aliases)) cmd.aliases.forEach((alias) => client.aliases.set(alias, cmd.name));
@@ -80,16 +82,16 @@ readdirSync("./SlashCommands/").forEach((dir) => {
   const slashFile = readdirSync(`./SlashCommands/${dir}/`).filter((files) => files.endsWith(".js"));
   for (const file of slashFile) {
     const SlashCommand = require(`./SlashCommands/${dir}/${file}`);
-    if(!SlashCommand.name) return console.error(`slashCommandNameError: ${SlashCommand.split(".")[0]} application command name is required.`);
-    if(!SlashCommand.description) return console.error(`slashCommandDescriptionError: ${SlashCommand.split(".")[0]} application command description is required.`);
+    if(!SlashCommand.name) return console.error(`[SLASH COMMANDS] NameError: ${SlashCommand.split(".")[0]} Application command name is required.`.brightRed);
+    if(!SlashCommand.description) return console.error(`[SLASH COMMANDS] DescriptionError: ${SlashCommand.split(".")[0]} Application command description is required.`.brightRed);
     client.slashCommands.set(SlashCommand.name, SlashCommand);
-    console.log(`${SlashCommand.name} SlashCommands registered to client`);
+    console.log(`[SLASH COMMANDS] ${SlashCommand.name} Application command is added for being registered to client`.brightMagenta);
     data.push(SlashCommand);
   }
 });
 
 client.on("ready", async () => {
-  await client.application.commands.set(data).then(() => console.log(`Client Commands Are Registered.`)).catch((e) => console.log(e));
+  await client.application.commands.set(data).then(() => console.log(`[SLASH COMMANDS] Application commands are registered to client.`.brightBlue)).catch((e) => console.log(e));
 })
 
 client.login(client.password)
